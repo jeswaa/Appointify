e23e4b0182a89a783ea1f04dd41bd492954a705a
jeswaa <jeswaaa1803@gmail.com>
2024-09-11 20:48:03 +0800

    My first commit

---
+++ FrontEnd/resources/views/Mainfolder/homepage.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appointify</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/css/homepage.css')}}">
+</head>
+<body>
+    <div class="top-section">
+        <img src="" alt="for edit muna ">
+        <div class="nav-links">
+            <a href="">Home</a>
+            <a href="">Features</a>
+            <a href="">Reviews</a>
+        </div>
+        <div class="login-tab">
+            <a href="{{route('Mainfolder.login')}}">Login</a>
+        </div>
+        <div class="signin-tab">
+            <a href="">Sign Up</a>
+        </div>
+    </div>
+
+    <div class="section1">
+        <h1>Simplify your Scheduling with <span>Appointify</span></h1>
+        <p>Effortless appointment management 
+        for businesses and clients alike.</p>
+    </div>
+
+    <div class="big-pic">
+        <img src="#" alt="Image here">
+    </div>
+
+    <button><a href="">GET STARTED NOW</a></button>
+</body>
+</html>
