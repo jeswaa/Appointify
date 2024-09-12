b2775e5ff431f36be9c22293fb0dc82ca0fd1e35
jeswaa <jeswaaa1803@gmail.com>
2024-09-13 00:09:48 +0800

    feat: login and register pages

--- FrontEnd/resources/views/Mainfolder/homepage.blade.php
+++ FrontEnd/resources/views/Mainfolder/homepage.blade.php
@@ -11,11 +11,11 @@
 </head>
 <body>
     <div class="top-section">
-        <img src="" alt="for edit muna ">
+        <img src="{{ asset('/images/logo.png')}}" alt="for edit muna ">
         <div class="nav-links">
-            <a href="">Home</a>
-            <a href="">Features</a>
-            <a href="">Reviews</a>
+            <a href="#section-1">Home</a>
+            <a href="#section-2">Features</a>
+            <a href="#section-3">Reviews</a>
         </div>
         <div class="login-tab">
             <a href="{{route('Mainfolder.login')}}">Login</a>
@@ -26,15 +26,67 @@
     </div>
 
     <div class="section1">
-        <h1>Simplify your Scheduling with <span>Appointify</span></h1>
-        <p>Effortless appointment management 
-        for businesses and clients alike.</p>
+        <section id="section-1">
+            <h1>Simplify your Scheduling with <span>Appointify</span></h1>
+            <p>Effortless appointment management 
+            for businesses and clients alike.</p>
+        </section>
     </div>
 
     <div class="big-pic">
-        <img src="#" alt="Image here">
+        <img src="{{ asset('/images/pic1.png')}}" alt="Image here">
     </div>
 
     <button><a href="{{route('Mainfolder.login')}}">GET STARTED NOW</a></button>
+
+    <section id="section-2">
+        <div class="middle-section">
+            <div class="container">
+                <div class="col-1">
+                    <h1>Why choose <span> APPOINTIFY </span>?</h1>
+                    <div class="card-container">
+                        <div class="card">
+                            <img src="{{asset('images/pic2.png')}}" alt="">
+                            <h1>easy booking</h1>
+                            <p>Simple and intuitive booking system</p>
+                        </div>
+                        <div class="card">
+                            <img src="{{asset('images/pic3.png')}}" alt="">
+                            <h1>automated reminders</h1>
+                            <p>Never miss an appointment with automated notifications</p>
+                        </div>
+                        <div class="card">
+                            <img src="{{asset('images/pic5.png')}}" alt="">
+                            <h1>calendar integration</h1>
+                            <p>Seamless sync with Google, Outlook, and other calendars.</p>
+                        </div>
+                        <div class="card">
+                            <img src="{{asset('images/pic4.png')}}" alt="">
+                            <h1>customizable options</h1>
+                            <p>Tailor your scheduling to fit your business needs.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <section id="section-3">
+                <div class="feedback-col">
+                        <h1>FOR EDIT PA ANG FEEDBACK SECTION</h1>
+                </div>
+            </section>
+        </div>
+    </section>
+
+    <footer>
+        <div class="left-col">
+            <p>Copyright @ 2024 . All right Reserved.</p>
+        </div>
+        <div class="social-media">
+            <a href="#">fb</a>
+            <a href="#">twitter</a>
+            <a href="#">ig</a>
+        </div>
+    </footer>
+
+    
 </body>
 </html>
